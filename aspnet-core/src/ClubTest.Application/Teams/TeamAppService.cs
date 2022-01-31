8c8f757a114f414574f48f7be13d2952c83b68e7
Moises Rivas <moises.rivas@metrodev.com.ar>
2022-01-31 20:27:23 -0300

    #Clase8 - Se agrego la implemantación de los metodos en TeamAppService haciendo uso de la implementación que tiene ABP de repositorios

--- aspnet-core/src/ClubTest.Application/Teams/TeamAppService.cs
+++ aspnet-core/src/ClubTest.Application/Teams/TeamAppService.cs
@@ -1,37 +1,57 @@
-﻿using ClubTest.Teams.Dtos;
-using System;
+﻿using Abp.Domain.Repositories;
+using ClubTest.Domain;
+using ClubTest.Teams.Dtos;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ClubTest.Teams
 {
     public class TeamAppService : ClubTestAppServiceBase, ITeamAppService
     {
-        public Task Create(TeamDto team)
+        private readonly IRepository<Team> _teamRepository;
+
+        public TeamAppService(IRepository<Team> teamRepository)
         {
-            throw new NotImplementedException();
+            _teamRepository = teamRepository;
         }
 
-        public Task Delete(int id)
+        public async Task Create(TeamDto team)
         {
-            throw new NotImplementedException();
+            var teamData = ObjectMapper.Map<Team>(team);
+            await _teamRepository.InsertAsync(teamData);
         }
 
-        public Task<TeamDto> Get(int id)
+        public async Task Delete(int id)
         {
-            throw new NotImplementedException();
+            var team = await _teamRepository.FirstOrDefaultAsync(id);
+            if (team != null)
+            {
+                await _teamRepository.DeleteAsync(team);
+            }
+        }
+
+        public async Task<TeamDto> Get(int id)
+        {
+            var team = await _teamRepository.FirstOrDefaultAsync(id);
+            var result = ObjectMapper.Map<TeamDto>(team);
+            return result;
         }
 
         public List<TeamDto> GetAll()
         {
-            throw new NotImplementedException();
+            var teams = _teamRepository.GetAll().Select(x => ObjectMapper.Map<TeamDto>(x)).ToList();
+            return teams;
         }
 
-        public Task Update(TeamDto team)
+        public async Task Update(TeamDto team)
         {
-            throw new NotImplementedException();
+            var teamData = await _teamRepository.FirstOrDefaultAsync(team.Id);
+            if (teamData != null)
+            {
+                teamData = ObjectMapper.Map(team, teamData);
+                await _teamRepository.UpdateAsync(teamData);
+            }
         }
     }
 }
